996a3817f2fccf71c5eb51639cd8e2327a406431
monishramj <monishramj621@gmail.com>
2025-08-11 13:55:00 -0500

    test: frame size fixes

--- test.py
+++ test.py
@@ -18,20 +18,21 @@ def view_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_
     frame_result = result
     # print('hand landmarker result: {}'.format(result))
 
-def draw_pts(result, img, w, h):
+def draw_pts(result, img):
     if result != None:
+        h, w, _ = img.shape
         marklist = result.hand_landmarks 
         for hand in marklist:
             for landmark in hand:
                 x = int(landmark.x * w); y = int(landmark.y * h)
-                cv.circle(img, (x, y), 5, (0, 255, 255), -1)
+                cv.circle(img, (x, y), 2, (0, 255, 255), 2)
     return
 
 options = HandLandmarkerOptions(base_options = BaseOptions(model_asset_path=model_path), running_mode = VisionRunningMode.LIVE_STREAM, result_callback=view_result, num_hands = 2, min_tracking_confidence = .6, min_hand_detection_confidence = .4)
 
 vid = cv.VideoCapture(0)
 
-WIDTH = 640
+WIDTH = 1000
 HEIGHT = 480
 
 vid.set(cv.CAP_PROP_FRAME_WIDTH, WIDTH)
@@ -52,7 +53,7 @@ with HandLandmarker.create_from_options(options) as landmarker:
 
         landmarker.detect_async(mp_image, ts_ms)
         
-        draw_pts(frame_result, frame, WIDTH, HEIGHT)
+        draw_pts(frame_result, frame)
 
         cv.imshow('frame', frame)
         if cv.waitKey(25) == ord('q'):
